7a84aa6b7cd5a199f8997af520c7e732a4cb228f
Keonseong <tttzkq9336@gmail.com>
2023-09-20 14:47:32 +0900

    password regex test

--- bank/src/test/java/shop/mtcoding/bank/temp/RegexTest.java
+++ bank/src/test/java/shop/mtcoding/bank/temp/RegexTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 // java.util.regex.Pattern
 public class RegexTest {
-    
+
     @Test
     public void 한글만가능_test() throws Exception {
         String value = "한글";
@@ -16,28 +16,38 @@ public class RegexTest {
 
     @Test
     public void 한글만불가능_test() throws Exception {
-        
+        String value = "abc";
+        boolean result = Pattern.matches("^[^ㄱ-ㅎ가-힣]*$", value);
+        System.out.println("테스트 : " + result);
     }
-    
+
     @Test
     public void 영어만가능_test() throws Exception {
-        
+        String value = "ssar";
+        boolean result = Pattern.matches("^[a-zA-Z]+$", value);
+        System.out.println("테스트 : " + result);
     }
 
     @Test
     public void 영어만불가능_test() throws Exception {
-        
+        String value = "abc";
+        boolean result = Pattern.matches("^[^a-zA-Z]*$", value);
+        System.out.println("테스트 : " + result);
     }
 
     @Test
     public void 영어와숫자만불가능_test() throws Exception {
-        
+        String value = "ssar3425";
+        boolean result = Pattern.matches("^[a-zA-Z0-9]+$", value);
+        System.out.println("테스트 : " + result);
     }
 
     @Test
     public void 영어만가능_길이는최소2최대4이다_test() throws Exception {
-        
+        String value = "ssar";
+        boolean result = Pattern.matches("^[a-zA-Z]{2,4}$", value);
+        System.out.println("테스트 : " + result);
     }
 
-    // username, email, fullname test
+    
 }
