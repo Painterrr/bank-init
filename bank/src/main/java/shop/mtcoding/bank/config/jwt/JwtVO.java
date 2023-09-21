163507982ee05564fd9eee2c96dfc5a66bcba954
Keonseong <tttzkq9336@gmail.com>
2023-09-21 10:14:32 +0900

    jwt Token Filter 구현 완료

---
+++ bank/src/main/java/shop/mtcoding/bank/config/jwt/JwtVO.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.bank.config.jwt;
+
+/*
+ * SECRET: 노출되면 안됨. (클라우드 AWS - 환경변수. 파일에 있는 것을 읽을 수도 있음)
+ * 리플래시 토큰: (X)
+ */
+public interface JwtVO {
+    public static final String SECRET = "메타코딩"; // HS256 (대칭키)
+    public static final int EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 7; // 1week
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER = "Authorization";
+
+}
--- bank/src/main/java/shop/mtcoding/bank/util/CustomResponseUtil.java
+++ bank/src/main/java/shop/mtcoding/bank/util/CustomResponseUtil.java
@@ -12,6 +12,19 @@ import shop.mtcoding.bank.dto.ResponseDto;
 public class CustomResponseUtil {
     private static final Logger log = LoggerFactory.getLogger(CustomResponseUtil.class);
     
+    public static void success(HttpServletResponse response, Object dto) {
+        try {
+            ObjectMapper om = new ObjectMapper();
+            ResponseDto<?> responseDto = new ResponseDto<>(-1, "로그인 성공", dto);
+            String responseBody = om.writeValueAsString(responseDto);
+            response.setContentType("application/json; charset=utf-8");
+            response.setStatus(200);
+            response.getWriter().println(responseBody);
+        } catch (Exception e) {
+            log.error("server parsing error");
+        }
+    }
+
     public static void unAuthentication(HttpServletResponse response, String msg) {
         try {
             ObjectMapper om = new ObjectMapper();
@@ -24,4 +37,6 @@ public class CustomResponseUtil {
             log.error("server parsing error");
         }
     }
+
+    
 }
