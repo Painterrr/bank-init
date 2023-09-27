7c778e61d207f2790d33b3fd157173879fc6d2ee
Keonseong <tttzkq9336@gmail.com>
2023-09-27 15:05:12 +0900

    Revert "계좌등록 서비스, 계좌등록 컨트롤러 생성."
    This reverts commit d2bc2af73f7973987dc8c6d8f86d2ba9e01f6c25.

--- bank/src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
+++ bank/src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AccountRepository extends JpaRepository<Account, Long>{
 
     // jpa query method
-    // checkpoint: 리펙토링 필요 (계좌 소유자 확인 시, 쿼리가 2번 생성되기 때문에 join fetch 필요)
+    // checkpoint: 리펙토링 필요
     Optional<Account> findByNumber(Long number);
     
 }
--- bank/src/main/java/shop/mtcoding/bank/service/AccountService.java
+++ bank/src/main/java/shop/mtcoding/bank/service/AccountService.java
@@ -15,8 +15,6 @@ import shop.mtcoding.bank.domain.account.Account;
 import shop.mtcoding.bank.domain.account.AccountRepository;
 import shop.mtcoding.bank.domain.user.User;
 import shop.mtcoding.bank.domain.user.UserRepository;
-import shop.mtcoding.bank.dto.account.AccountReqDto.AccountSaveReqDto;
-import shop.mtcoding.bank.dto.account.AccountRespDto.AccountSaveRespDto;
 import shop.mtcoding.bank.handler.ex.CustomApiException;
 
 @RequiredArgsConstructor
@@ -43,5 +41,40 @@ public class AccountService {
 
         // DTO 응답
         return new AccountSaveRespDto(accountPS);
+    }
+
+    @Getter
+    @Setter
+    public static class AccountSaveRespDto {
+        private Long id;
+        private Long number;
+        private Long balance;
+
+        public AccountSaveRespDto(Account account) {
+            this.id = account.getId();
+            this.number = account.getNumber();
+            this.balance = account.getBalance();
+        }
+    }
+
+    @Getter
+    @Setter
+    public static class AccountSaveReqDto {
+        @NotNull
+        @Digits(integer = 4, fraction = 4)
+        private Long number;
+        @NotNull
+        @Digits(integer = 4, fraction = 4)
+        private Long password;
+
+        public Account toEntity(User user) {
+            return Account.builder()
+                .number(number)
+                .password(password)
+                .balance(1000L)
+                .user(user)
+                .build();
+
+        }
     }
 }
