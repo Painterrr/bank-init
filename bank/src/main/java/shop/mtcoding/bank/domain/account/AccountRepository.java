7f9e980c12ff14f37331e926b422a44278c822ff
Keonseong <tttzkq9336@gmail.com>
2023-09-30 17:14:22 +0900

    본인 계좌목혹 보기 서비스 구현. ** 아래 스트림 문법 this.accounts = accounts.stream().map((account) -> new AccountDto(account)).collect(Collectors.toList()); this.accounts = accounts.stream().map(AccountDto::new).collect(Collectors.toList());

--- bank/src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
+++ bank/src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
@@ -1,5 +1,6 @@
 package shop.mtcoding.bank.domain.account;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,7 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AccountRepository extends JpaRepository<Account, Long>{
 
     // jpa query method.
-    // checkpoint: 리펙토링 필요. (계좌 소유자 확인 시, 쿼리가 2번 생성되기 때문에 join fetch 필요.)
+    // checkpoint: 리펙토링 필요. (계좌 소유자 확인 시, 쿼리가 2번 생성되기 때문에 join fetch 필요)
     Optional<Account> findByNumber(Long number);
     
+    // jpa query method.
+    // select * from account where user_id = :id
+    List<Account> findByUser_id(Long id);
 }
--- bank/src/main/java/shop/mtcoding/bank/service/AccountService.java
+++ bank/src/main/java/shop/mtcoding/bank/service/AccountService.java
@@ -1,12 +1,18 @@
 package shop.mtcoding.bank.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import shop.mtcoding.bank.domain.account.Account;
 import shop.mtcoding.bank.domain.account.AccountRepository;
 import shop.mtcoding.bank.domain.user.User;
@@ -21,6 +27,43 @@ import shop.mtcoding.bank.handler.ex.CustomApiException;
 public class AccountService {
     private final UserRepository userRepository;
     private final AccountRepository accountRepository;
+
+    public AccountListRespDto 계좌목록보기_유저별(Long userId) {
+        User userPS = userRepository.findById(userId).orElseThrow(
+            () -> new CustomApiException("user를 찾을 수 없습니다."));
+
+        // 해당 유저의 모든 계좌 목록
+        List<Account> accountListPS = accountRepository.findByUser_id(userId);
+        return new AccountListRespDto(userPS, accountListPS);
+    }
+
+    @Getter
+    @Setter
+    public static class AccountListRespDto {
+        private String fullanme;
+        private List<AccountDto> accounts = new ArrayList<>();
+        
+        public AccountListRespDto(User user, List<Account> accounts) {
+            this.fullanme = user.getFullname();
+            // this.accounts = accounts.stream().map((account) -> new AccountDto(account)).collect(Collectors.toList());
+            this.accounts = accounts.stream().map(AccountDto::new).collect(Collectors.toList());
+            // [account, account]
+        }
+        
+        @Getter
+        @Setter
+        public class AccountDto {
+            private Long id;
+            private Long number;
+            private Long balance;
+            
+            public AccountDto(Account account) {
+                this.id = account.getId();
+                this.number = account.getNumber();
+                this.balance = account.getBalance();
+            }
+        }
+    }
 
     @Transactional
     public AccountSaveRespDto 계좌등록(AccountSaveReqDto accountSaveReqDto, Long userId) {
