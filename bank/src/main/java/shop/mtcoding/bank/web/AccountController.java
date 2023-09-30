3542b051ff831f6035a685ceafec101797afdce9
Keonseong <tttzkq9336@gmail.com>
2023-09-30 18:36:13 +0900

    본인 계좌목록보기 컨트롤러 구현. ** 인증이 필요하고, account 테이블에서 {id}번 row를 주세요.(권한 처리 필요) -> 인증이 필요하고, account 테이블 데이터를 다 주세요. -> 인증이 필요하고, account 테이블에 login한 유저의 계좌만 주세요. @GetMapping("/s/account/{id}) -> -> @GetMapping("/s/account/login-user")

--- bank/src/main/java/shop/mtcoding/bank/web/AccountController.java
+++ bank/src/main/java/shop/mtcoding/bank/web/AccountController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,9 @@ import shop.mtcoding.bank.config.auth.LoginUser;
 import shop.mtcoding.bank.dto.ResponseDto;
 import shop.mtcoding.bank.dto.account.AccountReqDto.AccountSaveReqDto;
 import shop.mtcoding.bank.dto.account.AccountRespDto.AccountSaveRespDto;
+import shop.mtcoding.bank.handler.ex.CustomForbiddenException;
 import shop.mtcoding.bank.service.AccountService;
+import shop.mtcoding.bank.service.AccountService.AccountListRespDto;
 
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -29,5 +33,15 @@ public class AccountController {
             BindingResult bindingResult, @AuthenticationPrincipal LoginUser loginUser) {
         AccountSaveRespDto accountSaveRespDto = accountService.계좌등록(accountSaveReqDto, loginUser.getUser().getId());
         return new ResponseEntity<>(new ResponseDto<>(1, "계좌등록 성공", accountSaveRespDto), HttpStatus.CREATED);
+    }
+
+    // 인증이 필요하고, account 테이블에서 {id}번 row를 주세요.(권한 처리 필요)
+    // -> 인증이 필요하고, account 테이블 데이터를 다 주세요.
+    // -> 인증이 필요하고, account 테이블에 login한 유저의 계좌만 주세요.
+    @GetMapping("/s/account/login-user")
+    public ResponseEntity<?> findUserAccount(@AuthenticationPrincipal LoginUser loginUser) {
+
+        AccountListRespDto accountListRespDto = accountService.계좌목록보기_유저별(loginUser.getUser().getId());
+        return new ResponseEntity<>(new ResponseDto<>(1, "계좌목록보기_유저별 성공", accountListRespDto), HttpStatus.OK);
     }
 }
