606879ea6c91e951cdd9c8ad4ae6975a334ab34c
Keonseong <tttzkq9336@gmail.com>
2023-10-11 05:57:25 +0900

    jwt 토큰 만료시간 버그 처리.

--- bank/src/test/java/shop/mtcoding/bank/config/jwt/JwtProcessTest.java
+++ bank/src/test/java/shop/mtcoding/bank/config/jwt/JwtProcessTest.java
@@ -10,17 +10,25 @@ import shop.mtcoding.bank.domain.user.User;
 import shop.mtcoding.bank.domain.user.UserEnum;
 
 public class JwtProcessTest {
-    
+
+    private String createToken() {
+        // given
+        User user = User.builder().id(1L).role(UserEnum.ADMIN).build();
+        LoginUser loginUser = new LoginUser(user);
+
+        // when
+        String jwtToken = JwtProcess.create(loginUser);
+        return jwtToken;
+    }
+
     @Test
     public void create_test() throws Exception {
         // given
-        User user = User.builder().id(1L).role(UserEnum.CUSTOMER).build();
-        LoginUser loginUser = new LoginUser(user);
-    
+
         // when
-        String jwtToken = JwtProcess.create(loginUser);
+        String jwtToken = createToken();
         System.out.println("테스트 : " + jwtToken);
-    
+
         // then
         assertTrue(jwtToken.startsWith(JwtVO.TOKEN_PREFIX));
     }
@@ -28,13 +36,13 @@ public class JwtProcessTest {
     @Test
     public void verify_test() throws Exception {
         // given
-        String jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYW5rIiwicm9sZSI6IkNVU1RPTUVSIiwiaWQiOjEsImV4cCI6MTY5NjMyMjY4OH0.4-UUcH5dJtfd-LotbxNJVJrtsP7oo6BNSCwH2aF9pN1i76OqktIuZ8Rv2_WDFA38Zt33u6ksg7pT3yoT1K2TgQ";
-    
+        String token = createToken(); // Bearer 제거해서 처리.
+        String jwtToken = token.replace(JwtVO.TOKEN_PREFIX, "");
         // when
         LoginUser loginUser = JwtProcess.verify(jwtToken);
         System.out.println("테스트 : " + loginUser.getUser().getId());
-        System.out.println("테스트 : " + loginUser.getUser().getRole());
-    
+        System.out.println("테스트 : " + loginUser.getUser().getRole().name());
+
         // then
         assertThat(loginUser.getUser().getId()).isEqualTo(1L);
         assertThat(loginUser.getUser().getRole()).isEqualTo(UserEnum.ADMIN);
