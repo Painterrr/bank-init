0e10ebfe8ae80a87c63bc6dc20617c26aed2cc1e
Keonseong <tttzkq9336@gmail.com>
2023-10-13 17:09:14 +0900

    입출금내역조회 서비스 생성.

---
+++ bank/src/main/java/shop/mtcoding/bank/dto/transaction/TransactionRespDto.java
@@ -0,0 +1,62 @@
+package shop.mtcoding.bank.dto.transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lombok.Getter;
+import lombok.Setter;
+import shop.mtcoding.bank.domain.account.Account;
+import shop.mtcoding.bank.domain.transaction.Transaction;
+import shop.mtcoding.bank.util.CustomDateUtil;
+
+public class TransactionRespDto {
+    @Getter
+    @Setter
+    public static class TransactionListRespDto {
+        private List<TransactionDto> transactions = new ArrayList<>();
+
+        public TransactionListRespDto(List<Transaction> transactions, Account account) {
+            this.transactions = transactions.stream()
+                    .map((transaction) -> new TransactionDto(transaction, account.getNumber()))
+                    .collect(Collectors.toList());
+        }
+
+        @Setter
+        @Getter
+        public class TransactionDto {
+            private Long id;
+            private String gubun;
+            private Long amount;
+            private String sender;
+            private String receiver;
+            private String tel;
+            private String createdAt;
+            private Long balance;
+
+            public TransactionDto(Transaction transaction, Long accountNumber) {
+                this.id = transaction.getId();
+                this.gubun = transaction.getGubun().getValue();
+                this.amount = transaction.getAmount();
+                this.sender = transaction.getSender();
+                this.receiver = transaction.getReceiver();
+                this.createdAt = CustomDateUtil.toStringFormat(transaction.getCreatedAt());
+                this.tel = transaction.getTel() == null ? "없음" : transaction.getTel();
+
+                // 계좌의 입출금 내역: (출금계좌 == null, 입금계좌 == 값) or (출금계좌 == 값, 입금계좌 == null)
+                if (transaction.getDepositAccount() == null) {
+                    this.balance = transaction.getWithdrawAccountBalance();
+                } else if (transaction.getWithdrawAccount() == null) {
+                    this.balance = transaction.getDepositAccountBalance();
+                } else {
+                    // 계좌의 입출금 내역: (출금계좌 == 값, 입금계좌 == 값)
+                    if (accountNumber.longValue() == transaction.getDepositAccount().getNumber()) {
+                        this.balance = transaction.getDepositAccountBalance();
+                    } else {
+                        this.balance = transaction.getWithdrawAccountBalance();
+                    }
+                }
+            }
+        }
+    }
+}
--- bank/src/main/java/shop/mtcoding/bank/service/TransactionService.java
+++ bank/src/main/java/shop/mtcoding/bank/service/TransactionService.java
@@ -1,19 +1,38 @@
 package shop.mtcoding.bank.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import shop.mtcoding.bank.domain.account.Account;
+import shop.mtcoding.bank.domain.account.AccountRepository;
+import shop.mtcoding.bank.domain.transaction.Transaction;
 import shop.mtcoding.bank.domain.transaction.TransactionRepository;
+import shop.mtcoding.bank.dto.transaction.TransactionRespDto.TransactionListRespDto;
+import shop.mtcoding.bank.handler.ex.CustomApiException;
 
 @RequiredArgsConstructor
 @Transactional
 @Service
 public class TransactionService {
     private final TransactionRepository transactionRepository;
+    private final AccountRepository accountRepository;
 
-    public void 입출금목록보기(Long userId, Long accountNumber) {
-        
+    public TransactionListRespDto 입출금목록보기(Long userId, Long accountNumber, String gubun, int page) {
+        Account accountPS = accountRepository.findByNumber(accountNumber)
+                .orElseThrow(() -> new CustomApiException("해당 계좌를 찾을 수 없습니다."));
+
+        accountPS.checkOwner(userId);
+
+        List<Transaction> transactionListPS = transactionRepository.findTransactionList(accountPS.getId(), gubun, page);
+
+        return new TransactionListRespDto(transactionListPS, accountPS);
     }
+
+    
 }
